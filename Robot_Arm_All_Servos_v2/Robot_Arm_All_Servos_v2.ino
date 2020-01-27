e8f6743dddedc1049694e8cfe357df8f177290f0
Jonathan Shulgach <45723019+Jshulgach@users.noreply.github.com>
2020-01-26 20:21:13 -0500

    Add files via upload

--- Robot_Arm_All_Servos_v2/Robot_Arm_All_Servos_v2.ino
+++ Robot_Arm_All_Servos_v2/Robot_Arm_All_Servos_v2.ino
@@ -1,10 +1,17 @@
 /*
+ * 
 Robot Arm Sweep and Retract_1_25_20
 
-Moves all servos with specific times and rotations
+Moves all servos within a system based on positional values stored in a multidimmensional array. The position array is 6 rows long, 
+and the length is determined by duration of the entire servo motion set. In this case, 6 servos will create a row each for
+positional array correspondign to that servo. In the actionArray variable, information pertaining to certain "actions" are 
+collected with the time to start movement, name of servo, start/end positions and start/end times. The duration of the servo 
+action, along with the frequency of data being written to the servos, creates a "linspace" of numbers interpolatd from starting to
+ending positions within the array, according to when the action time happens within the overal duration of the actions
 
-Modified 25th January 2020
+Modified 26th January 2020
 by Jonathan Shulgach
+
  */
 
 #include <Servo.h>
@@ -20,33 +27,38 @@ int frequency = 10;
 long previousTime = 0;
 int max_val = 0;
 int max_idx = 0;
-int total_duration = 0;
+unsigned long compare = 0;
+int val = 0;
+
+String uniqueServos[] = {"Torso","Shoulder","Elbow","Claw", "WristRot","WristBend"};
 
 struct ServoAction
 {
   unsigned long actionTime; // Number of milliseconds after sequence starts to do this action
   Servo ServoToAct;         // Which servo is it?
-  int startPos;         // Number of degrees to send the designated servo to
-  int endPos;           // Number of degrees to send the designated servo to
+  String servoName;           // Name of servo (need this for now)
+  int startPos;             // Number of degrees to send the designated servo to
+  int endPos;               // Number of degrees to send the designated servo to
   int startTime;            // Start time
   int endTime;              // End time
 };
 
 ServoAction actionArray[] = {
-  {    0,       Torso,     0,    90,     0,    1,}, // When sequence starts, send Torso to 90 degrees
-  { 1000,    Shoulder,     0,    45,     0,    1,},
-  { 1000,        Claw,   180,   180,     0,    1,},
-  { 2000,       Elbow,     0,     0,     0,    1,}, 
-  { 9000,    WristRot,     0,    90,     0,    1,},
-  {10000,   WristBend,     0,     0,     0,    1,},
-  {10000,       Torso,     0,     0,     0,    1,},
-  {11000,       Torso,   180,    90,     0,    1,},
+  {    0,       Torso,     "Torso",     0,    90,     0,    1,}, // When sequence starts, send Torso to 90 degrees
+  {    0,    Shoulder,  "Shoulder",     0,    45,     0,    1,},
+  {    0,       Elbow,     "Elbow",    90,   180,     0,    1,},
+  {    0,        Claw,      "Claw",     0,    90,     0,    1,}, 
+  { 9000,    WristRot,  "WristRot",     0,    90,     0,    1,},
+  {10000,   WristBend, "WristBend",     90,   90,     0,    1,},
+  {10000,       Torso,     "Torso",     90,  180,     0,    1,},
+  {11000,       Torso,     "Torso",   180,    90,     0,    1,},
   /* etc. etc. */
 };
 
-const byte N_Actions = sizeof actionArray / sizeof actionArray[0];
+int N_Actions = sizeof actionArray / sizeof actionArray[0];
+int N_Servos = sizeof uniqueServos / sizeof uniqueServos[0];
 
-int initialize() {
+void initialize() {
   // Initialize whole array with largest size needed
   for (int k=0; k<N_Actions; k += 1){
     if (actionArray[k].actionTime > max_val){
@@ -57,54 +69,43 @@ int initialize() {
   int max_delta_t = abs(actionArray[max_idx].endTime-actionArray[max_idx].startTime);
   static unsigned long total_duration = actionArray[max_idx].actionTime+round(max_delta_t*1000);
   unsigned long N_elem = round(total_duration*frequency/1000);
-  unsigned long stepsize = (1000/frequency);
-  Serial.print("step size: "); Serial.println(stepsize);
-  Serial.print("max delta: "); Serial.println(max_delta_t);
-  Serial.print("total duration: "); Serial.println(total_duration);
-  unsigned int pos_Array[N_Actions][N_elem]={}; //Array creation
-
-  Serial.print("Array size: "); Serial.print(N_Actions); Serial.print(" by "); Serial.println(N_elem);
-  // Fill in array with padded values to start 
-  for (int k=0; k<N_Actions; k += 1){
-    unsigned long action_time = actionArray[k].actionTime;
-    Serial.print(action_time); 
-    if (action_time > 0){
-//      for (long i=0; i<actionArray[k].actionTime; i += stepsize){
-//        Serial.print(actionArray[k].startTime); Serial.print(" ");
-//        pos_Array[k][i] = actionArray[k].startTime;
-//      }
-//      Serial.println(": not zero ");
-//    } else{
-    Serial.println(": zero");
-    }
-//    // Create the positional values from linspace
-//    int delta_t = abs(actionArray[k].endTime-actionArray[k].startTime);
-//    int N_incr = round(delta_t*frequency);
-//    for (int j=1; j <= N_incr; j+=1){
-//      Serial.print(actionArray[k].startPos + (j-1)*(actionArray[k].endPos-actionArray[k].startPos)/(N_incr-1));
-//      Serial.println(" ");
-//      pos_Array[k][actionArray[k].startPos + j] = actionArray[k].startPos + (j-1)*(actionArray[k].endPos-actionArray[k].startPos)/(N_incr-1);
-//    }
-//    Serial.println(" ");
-//    
-//    // Finish padding at end
-//    if (actionArray[k].endTime < max_val){
-//      for (int i=max_val; i> actionArray[k].endTime; i-=stepsize){
-//        Serial.print(actionArray[k].endTime); Serial.println(" ");
-//        pos_Array[k][i] = actionArray[k].endTime;
-//      }
-//      Serial.println(" ");
-//    }
-//    for (int m=0; m<total_duration; m+=1){
-//      Serial.print("Servo: ");
-//      Serial.println(k);
-//      Serial.print("Position array: ");
-//      Serial.print(pos_Array[k][m]); Serial.print(" ");
-//      Serial.println(" ");
-//    }
-  }
   
-  //return pos_Array;
+  Serial.print( F("Frequency: ") ); Serial.println(frequency);
+  Serial.print( F("max delta: ") ); Serial.println(max_delta_t);
+  Serial.print( F("total duration: ") ); Serial.println(total_duration);
+  Serial.print( F("Array size: ") ); Serial.print(N_Servos); Serial.print(" by "); Serial.println(N_elem); Serial.println(" ");
+  
+  unsigned int test_pos[N_Servos][N_elem]={};
+  //unsigned int test_pos[N_elem]={};
+
+  Serial.println( F("Servos: ") );
+  for (int n=0; n<N_Servos; n += 1){ 
+    Serial.println(uniqueServos[n]);
+    
+    for (int k=0; k<N_Actions; k += 1){
+      if (actionArray[k].servoName==uniqueServos[n]){
+        int delta_t = abs(actionArray[k].endTime-actionArray[k].startTime);
+        int N_incr = round(delta_t*frequency);
+        int elem = actionArray[k].actionTime*frequency/1000;
+      
+        for (int i=0; i < N_incr; i+=1){
+          test_pos[n][elem + i] = actionArray[k].startPos + (i)*(actionArray[k].endPos-actionArray[k].startPos)/(N_incr-1);
+        }
+        for (int i=(elem + N_incr); i<N_elem; i+=1){
+          test_pos[n][i] = actionArray[k].endPos;
+        }
+      }
+    }                                                                       
+  }
+
+  Serial.println(" ");
+  Serial.println( F("Position array: ") );
+  for (int n=0; n<N_Servos; n += 1){ 
+    for (int i=0; i<N_elem; i += 1){
+      Serial.print(test_pos[n][i]); Serial.print(" ");
+    }
+    Serial.println(" ");
+  }
 }
 
 //void execute_Action(int pos_Array, int N_Actions){
@@ -129,16 +130,14 @@ void setup() {
   WristRot.write(90);
   WristBend.attach(6);
   Claw.attach(7);
-  Serial.begin(9600);
+  Serial.begin(115200);
 
-
-  int *p_array;
-  p_array = initialize();
+  //int *p_array;
+  initialize();
   //execute_action(p_array, N_Actions);
   
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
 }
