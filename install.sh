59868f4c24353dc58b223991e3ff0f06616f4bbd
Jonathan Shulgach <45723019+Jshulgach@users.noreply.github.com>
2022-10-17 18:27:38 -0400

    sourcing at the very end

--- install.sh
+++ install.sh
@@ -22,10 +22,7 @@ sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 # locale  # verify settings
-sudo apt-get install -y \
-    curl \
-    gnupg2 \
-    lsb-release
+sudo apt-get install -y curl gnupg2 lsb-release
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 # sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
 sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/nullsudo apt-get update
@@ -41,15 +38,7 @@ fi
 source /opt/ros/foxy/setup.bash
 echo "source /opt/ros/foxy/setup.bash" >> ~/.bashrc
 
-sudo apt-get install -y \
-    cmake \
-    build-essential \ 
-    python3-pip \ 
-    python3-colcon-common-extensions \
-    python3-argcomplete \
-    python3-rosdep \
-    python3-setuptools \
-
+sudo apt-get install -y cmake build-essential python3-pip python3-colcon-common-extensions python3-argcomplete python3-rosdep python3-setuptools
 sudo rosdep init
 rosdep update
 
@@ -65,18 +54,14 @@ sudo apt dist-upgrade -y
 # Install build tools and moveit2
 
 
-sudo apt-get install -y \
-    ros-foxy-moveit \
-    ros-foxy-ros2-control \
-    ros-foxy-ros2-controllers \
-    
-    python3-vcstool \
-
+sudo apt-get install -y ros-foxy-moveit ros-foxy-ros2-control ros-foxy-ros2-controllers python3-vcstool
 vcs import < Desktop-Arm/desktop_arm.repos
 
 # Finally build workspace and any dependencies
 cd ~/ros2_ws
 rosdep install -r --from-paths . --ignore-src --rosdistro $ROS_DISTRO -y
-MAKEFLAGS="-j1 -l1" colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release --symlink-install --packages-select desktop_arm
-
-
+olcon build --cmake-args -DCMAKE_BUILD_TYPE=Release --symlink-install
+source ~/ros2_ws/install/setup.bash
+echo "~/ros2_ws/install/setup.bash" >> ~/.bashrc
+sudo apt-get update
+sdo apt-get upgrade -y
