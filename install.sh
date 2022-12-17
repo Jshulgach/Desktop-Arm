28f833c414c391c38644d165ece51d04b88b2b24
Jonathan Shulgach <45723019+Jshulgach@users.noreply.github.com>
2022-12-16 22:29:14 -0500

    Setup now works on Ubuntu 22.04, with ROS2 Humble and Moveit2!

--- install.sh
+++ install.sh
@@ -17,63 +17,51 @@ fi
 cd ~
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y locales
+sudo apt-get install -y 
+
+locale  # check for UTF-8
+sudo apt update && sudo apt install locales
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
-# locale  # verify settings
-sudo apt-get install -y curl gnupg2 lsb-release
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-# sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
-sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/nullsudo apt-get update
-sudo apt-get upgrade -y'
+
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+
+sudo apt update && sudo apt install curl
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt-get update; sudo apt-get upgrade -y
 
 if [ "$VERSION" = "1" ]; then
-  sudo apt-get install -y ros-foxy-desktop
+  sudo apt-get install -y ros-humble-desktop
 else
-  sudo apt-get install -y ros-foxy-ros-base
+  sudo apt-get install -y ros-humble-ros-base
 fi
 
-# Add source and update rosdep
-source /opt/ros/foxy/setup.bash
-echo "source /opt/ros/foxy/setup.bash" >> ~/.bashrc
+# Install development tools: Compilers and other tools to build ROS packages
+sudo apt install ros-dev-tools
+sudo apt install python3-colcon-common-extensions
 
-<<<<<<< HEAD
-sudo apt-get install -y cmake build-essential python3-pip python3-colcon-common-extensions python3-argcomplete python3-rosdep python3-setuptools
-=======
-sudo apt-get install -y \
-    cmake \
-    build-essential \ 
-    python3-pip \ 
-    python3-colcon-common-extensions \
-    python3-argcomplete \
-    python3-rosdep
-     \
-    python3-setuptools \
-
->>>>>>> ros2
-sudo rosdep init
+# Add source to shell startup script and update rosdep
+source /opt/ros/humble/setup.bash
+echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+sudo rosdep init 
 rosdep update
-
-# Add colcon_cd to your shell startup script
-source /usr/share/colcon_cd/function/colcon_cd.sh
-export _colcon_cd_root=~/ros2_install
-echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
-echo "export _colcon_cd_root=~/ros2_install" >> ~/.bashrc
 
 sudo apt-get update
 sudo apt dist-upgrade -y
 
 # Install build tools and moveit2
-
-
-sudo apt-get install -y ros-foxy-moveit ros-foxy-ros2-control ros-foxy-ros2-controllers python3-vcstool
+sudo apt-get install -y ros-humble-moveit
+sudo apt-get install -y ros-humble-ros2-control ros-humble-ros2-controllers
 vcs import < Desktop-Arm/desktop_arm.repos
 
 # Finally build workspace and any dependencies
 cd ~/ros2_ws
 rosdep install -r --from-paths . --ignore-src --rosdistro $ROS_DISTRO -y
-olcon build --cmake-args -DCMAKE_BUILD_TYPE=Release --symlink-install
+colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release --symlink-install
 source ~/ros2_ws/install/setup.bash
 echo "~/ros2_ws/install/setup.bash" >> ~/.bashrc
 sudo apt-get update
